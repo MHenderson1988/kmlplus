40b0eb4ab86d4b824bb022de6702e1b56c9b3082
Mark Henderson <mark.henderson1988@gmail.com>
2020-12-29 21:58:19 +0000

    Added setters and getters for coordinate class

--- kmlplus/coordinates.py
+++ kmlplus/coordinates.py
@@ -3,13 +3,16 @@ from geographiclib.geodesic import Geodesic
 from geopy import distance as gp
 
 # prepend regex pattern with 'r' to show it is a raw string
-coordinate_regex = r'^[-+]?([1-8]?\d(\.\d+)?|90(\.0+)?),\s*[-+]?(180(\.0+)?|((1[0-7]\d)|([1-9]?\d))(\.\d+)?)$'
+decimal_pattern = r'^-?([1-8]?[1-9]|[1-9]0)\.{1}\d{1,6}'
+nats_dms_patten = r'''^s*([+-]?\d{1,3}\*?\s+\d{1,2}'?\s+\d{1,2}"?[NSEW]?|\d{1,3}(:\d{2}){2}\.\d[NSEW]\s*){1,2}$'''
 
 
 class Coordinate:
-    def __init__(self, latitude, longitude):
-        self._latitude = latitude
-        self._longitude = longitude
+    def __init__(self, lat, long, height, coordinate_type):
+        self._latitude = lat
+        self._longitude = long
+        self._height = height
+        self._coordinate_type = coordinate_type
 
     @property
     def latitude(self):
@@ -17,10 +20,14 @@ class Coordinate:
 
     @latitude.setter
     def latitude(self, a_latitude):
-        if re.match(coordinate_regex, a_latitude):
+        if re.match(decimal_pattern, a_latitude):
             self._latitude = a_latitude
         else:
-            ValueError("Latitude must be a valid integer if using DMS coordinates")
+            print(ValueError("Latitude must be a valid integer if using DMS coordinates"))
+
+    @latitude.deleter
+    def latitude(self):
+        del self._latitude
 
     @property
     def longitude(self):
@@ -28,10 +35,29 @@ class Coordinate:
 
     @longitude.setter
     def longitude(self, a_longitude):
-        if re.match(coordinate_regex, a_longitude):
+        if re.match(decimal_pattern, a_longitude):
             self._longitude = a_longitude
         else:
-            ValueError("Latitude must be a valid integer if using DMS coordinates")
+            print(ValueError("Latitude must be a valid integer if using DMS coordinates"))
+
+    @longitude.deleter
+    def longitude(self):
+        del self._longitude
+
+    @property
+    def height(self):
+        return self._height
+
+    @height.setter
+    def height(self, a_height):
+        if a_height.isinstance(float) or a_height.isinstance(int):
+            self._height = a_height
+        else:
+            print(ValueError("Height must be a valid floating point number eg -5.3"))
+
+    @height.deleter
+    def height(self):
+        del self._height
 
     def to_string(self):
         the_string = "{}, {}".format(self.latitude, self.longitude)
--- test/test_coordinates.py
+++ test/test_coordinates.py
@@ -4,5 +4,13 @@ from kmlplus.coordinates import Coordinate
 
 class TestCoordinates(TestCase):
     def test_coordinate(self):
-        c1 = Coordinate(55.21434, -4.38487)
-        print(c1.__dict__)
+        latitude_list = ["55.11", "55.11111234", "55.1232", "55.37823782"]
+        c1 = Coordinate("55.21434", "-4.38487", 0, coordinate_type="dms")
+        for latitude in latitude_list:
+            c1.latitude = latitude
+            self.assertEqual(c1.latitude, latitude)
+
+        longitude_list = ["-4.37363", "-3.0", "-5.43", "5.34873478"]
+        for longitude in longitude_list:
+            c1.longitude = longitude
+            self.assertEqual(c1.longitude, longitude)
