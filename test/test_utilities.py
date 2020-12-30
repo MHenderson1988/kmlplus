9ebab2cce198e92939eeccdf7fd70636381bf8f3
Mark Henderson <mark.henderson1988@gmail.com>
2020-12-30 09:23:47 +0000

    Started adding test for utilities getbearing function

--- test/test_utilities.py
+++ test/test_utilities.py
@@ -6,6 +6,14 @@ from kmlplus.utilities import get_bearing
 
 class TestUtilities(TestCase):
     def test_get_bearing(self):
-        c1 = Coordinate(55.11, -4.11, 0, coordinate_type='decimal')
-        c2 = Coordinate(55.22, -4.22, 0, coordinate_type='decimal')
-        print(get_bearing(c1, c2))
+        list_of_coordinates = [(55.11, -4.11), (55.22, -4.22), (54.11, 4.11), (54.22, 4.22)]
+        reversed_list_of_coordinates = [coordinates[::-1] for coordinates in list_of_coordinates]
+        expected_results = []
+        c1 = Coordinate(1, 1, 0, coordinate_type='decimal')
+        c2 = Coordinate(1, 1, 0, coordinate_type='decimal')
+        i = 0
+        while i < len(list_of_coordinates):
+            c1.latitude, c1.longitude = list_of_coordinates[i][0], list_of_coordinates[i][1]
+            c2.latitude, c1.longitude = reversed_list_of_coordinates[i][0], reversed_list_of_coordinates[i][1]
+            print(get_bearing(c1, c2))
+            i += 1
