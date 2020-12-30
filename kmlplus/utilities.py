ce5f6388e1026c355ba911d805448c2c30f290f2
Mark Henderson <mark.henderson1988@gmail.com>
2020-12-30 08:53:01 +0000

    initial commit

---
+++ kmlplus/utilities.py
@@ -0,0 +1,9 @@
+from geographiclib.geodesic import Geodesic
+from geopy import distance as gp
+
+"""
+This function takes 2 arguments of the coordinate class type and calculates the bearing between the two locations.
+This is used in the creation of arcs/circles.
+"""
+
+def get_bearing()
