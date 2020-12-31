23d1f6b17aa3c411cbf6e4497d4202a93e1bb5ce
Mark Henderson <mark.henderson1988@gmail.com>
2020-12-31 14:37:54 +0000

    Added tests for get_bearing

--- test/test_utilities.py
+++ test/test_utilities.py
@@ -5,15 +5,17 @@ from kmlplus.utilities import get_bearing
 
 
 class TestUtilities(TestCase):
+    def setUp(self):
+        self.coordinates_1 = [(55.11, -4.11), (55.22, -4.22), (54.11, 4.11), (54.22, 4.22)]
+        self.coordinates_2 = [(52.32, -4.45), (55.75, -4.11), (53.453, -4.8587), (56.2832, -2.4893)]
+
     def test_get_bearing(self):
-        list_of_coordinates = [(55.11, -4.11), (55.22, -4.22), (54.11, 4.11), (54.22, 4.22)]
-        reversed_list_of_coordinates = [coordinates[::-1] for coordinates in list_of_coordinates]
         expected_results = []
         c1 = Coordinate(1, 1, 0, coordinate_type='decimal')
         c2 = Coordinate(1, 1, 0, coordinate_type='decimal')
         i = 0
-        while i < len(list_of_coordinates):
-            c1.latitude, c1.longitude = list_of_coordinates[i][0], list_of_coordinates[i][1]
-            c2.latitude, c1.longitude = reversed_list_of_coordinates[i][0], reversed_list_of_coordinates[i][1]
+        while i < len(self.coordinates_1):
+            c1.latitude, c1.longitude = self.coordinates_1[i][0], self.coordinates_1[i][1]
+            c2.latitude, c1.longitude = self.coordinates_2[i][0], self.coordinates_2[i][1]
             print(get_bearing(c1, c2))
             i += 1
