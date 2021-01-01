fab2247f44a93c62017c88b607695237c5cd0243
Mark Henderson <mark.henderson1988@gmail.com>
2021-01-01 17:01:37 +0000

    Part way through adding tests and function for dms to decimal

--- kmlplus/coordinates.py
+++ kmlplus/coordinates.py
@@ -43,18 +43,33 @@ class Coordinate:
         dms_string = str(degrees) + str(int(minutes)) + str(round(seconds))
         return int(dms_string)
 
+    @staticmethod
+    def dms_to_decimal(coordinate_to_convert):
+        degrees = int(str(coordinate_to_convert)[0:2])
+        minutes = float(str(coordinate_to_convert)[2:4]) / 60
+        seconds = float(str(coordinate_to_convert)[4:]) / 3600
+        decimal_degrees = float(degrees + (minutes + seconds))
+        return decimal_degrees
+
     def to_string(self):
         the_string = "{}, {}, {}".format(self._latitude, self._longitude, self._height)
         return the_string
 
     def convert_to_dms(self):
         if self.coordinate_type == 'dms':
-            return TypeError("The coordinates provided are already in the degrees, minutes, seconds format.  You need "
-                             "to call the convert_to_decimal function instead")
+            raise TypeError("The coordinates provided are already in the degrees, minutes, seconds format.  You need "
+                            "to call the convert_to_decimal function instead")
         else:
-            # Convert latitude
             self._latitude = self.decimal_to_dms(self._latitude)
             self._longitude = self.decimal_to_dms(self._longitude)
+
+    def convert_to_decimal(self):
+        if self.coordinate_type == 'decimal':
+            raise TypeError("The coordinates are already decimal format.  Either call the convert to dms function OR"
+                            "check you have the correct coordinates set")
+        else:
+            self._latitude = self.dms_to_decimal(self._latitude)
+            self._longitude = self.dms_to_decimal(self._longitude)
 
     """This takes an instance of the Coordinate class as its argument.  It returns the bearing (of type float) from the
     instance which is calling the function to the instance provided in the argument"""
--- test/test_coordinates.py
+++ test/test_coordinates.py
@@ -8,11 +8,12 @@ class TestCoordinates(TestCase):
     def setUpClass(cls):
         cls._c1 = Coordinate(55.38327, -4.32723, 0, coordinate_type="decimal")
         cls._c2 = Coordinate(0, 0, 0, coordinate_type="decimal")
+        cls._c3 = Coordinate(554323, -47543, 0, coordinate_type="dms")
 
     def setUp(self):
         self.decimal_coordinates_1 = [(55.11, -4.11), (55.22, -4.22), (54.11, 4.11), (54.22, 4.22)]
         self.decimal_coordinates_2 = [(52.32, -4.45), (55.75, -4.11), (53.453, -4.8587), (56.2832, -2.4893)]
-        self.dms_coordinates_1 = [(55636, -4636), (551312, -41312), (54636, 4636), (541312, 41312)]
+        self.dms_coordinates_1 = [(546512, -25545), (556545, -45210), (505425, 42215), (540012, 40214)]
 
     def test_coordinate(self):
         for coordinate_pair in self.decimal_coordinates_1:
@@ -39,4 +40,18 @@ class TestCoordinates(TestCase):
             self._c1.convert_to_dms()
             self.assertEqual(self._c1.latitude, self.dms_coordinates_1[i][0])
             self.assertEqual(self._c1.longitude, self.dms_coordinates_1[i][1])
+            # Check coordinate type of dms raises a type error
+            with self.assertRaises(TypeError):
+                self._c3.convert_to_dms()
             i += 1
+
+    def test_dms_to_decimal(self):
+        i = 0
+        expected = [(55.08667, -2.929167), (56.09583333, -4.8694444), (55.90694444, 4.37083333),
+                    (54.00333333, 4.03722222)]
+        while i < len(self.dms_coordinates_1):
+            self._c3.latitude, self._c3.longitude = self.dms_coordinates_1[i][0], self.dms_coordinates_1[i][1]
+            self._c3.convert_to_decimal()
+            print(self._c3.to_string())
+            self.assertAlmostEqual(self._c3.latitude, expected[i][0])
+            self.assertAlmostEqual(self._c3.longitude, expected[i][1])
