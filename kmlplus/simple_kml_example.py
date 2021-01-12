1a11d909d62a84880d0db8651588dca7b3a0345b
Mark Henderson <mark.henderson1988@gmail.com>
2021-01-12 11:35:11 +0000

    Fixed side coding for straight linepaths.  All four sides now render correctly

--- kmlplus/simple_kml_example.py
+++ kmlplus/simple_kml_example.py
@@ -1,17 +1,17 @@
 import simplekml
-from kmlplus import paths, coordinates, floatingpolygon
+from kmlplus import paths, coordinates
 
 a_list = [(55.11, -4.11), (55.22, -4.11), (55.22, -4.22), (55.11, -4.22)]
 instance_list = [coordinates.Coordinate(x[0], x[1], height=2000) for x in a_list]
 instance_list_higher = [coordinates.Coordinate(x[0], x[1], height=6000) for x in a_list]
-lower_surface, upper_surface = paths.LinePath(*instance_list, sort=True), paths.LinePath(*instance_list_higher, sort=True)
-floating_poly = floatingpolygon.FloatingPolygon(lower_surface, upper_surface)
+lower_surface, upper_surface = paths.LinePath(*instance_list, sort=True), paths.LinePath(*instance_list_higher,
+                                                                                         sort=True)
+lower_surface.create_sides(upper_surface)
 
 
 def create_kml():
     kml = simplekml.Kml()
     fol = kml.newfolder(name="Example polygon")
-    sides = floating_poly.sides
 
     pol = fol.newpolygon()
     pol.outerboundaryis = lower_surface.kml_coordinate_list
@@ -21,9 +21,13 @@ def create_kml():
     pol.outerboundaryis = upper_surface.kml_coordinate_list
     pol.altitudemode = simplekml.AltitudeMode.relativetoground
 
-    pol = fol.newpolygon()
-    pol.outerboundaryis = sides.kml_coordinate_list
-    pol.altitudemode = simplekml.AltitudeMode.relativetoground
+    i = 0
+    for coord_lists in lower_surface.sides:
+        pol = fol.newpolygon()
+        pol.outerboundaryis = coord_lists
+        pol.altitudemode = simplekml.AltitudeMode.relativetoground
+        i += 1
+
 
     kml.save('Floating polygon example.kml')
 
