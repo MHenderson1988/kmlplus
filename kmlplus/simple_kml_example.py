f6b73b7b521635f52109c1b5c2e9c4969720f3f1
Mark Henderson <mark.henderson1988@gmail.com>
2021-01-13 11:37:11 +0000

    Added comments to the example kml file

--- kmlplus/simple_kml_example.py
+++ kmlplus/simple_kml_example.py
@@ -1,12 +1,30 @@
 import simplekml
+
 from kmlplus import paths, coordinates
 
+# Create a list of tuples in the coordinate format you wish.  DMS coordinates should be provided in DDMMSS format
+# without any decimal places.  All coordinates should be presented in the Y, X or latitude, longitude format.
+
 a_list = [(55.11, -4.11), (55.22, -4.11), (55.22, -4.22), (55.11, -4.22)]
-instance_list = [coordinates.Coordinate(x[0], x[1], height=2000) for x in a_list]
+
+# You can iterate through your list of tuples and create instances of the Coordinate object.  The coordinate object
+# will accept a 'height' kwarg.
+
+instance_list_lower = [coordinates.Coordinate(x[0], x[1], height=2000) for x in a_list]
 instance_list_higher = [coordinates.Coordinate(x[0], x[1], height=6000) for x in a_list]
-lower_surface, upper_surface = paths.LinePath(*instance_list, height=8000, sort=True),\
+
+# Using your list of Coordinate instances, you can create instances of the LinePath object.  The LinePath argument
+# will overwrite ALL coordinate instance height information if the LinePath takes a 'height' kwarg.  When the 'sort'
+# kwarg is given as True (Default, False), the coordinates will be rearranged in anticlockwise order.  This is to allow
+# for correct rendering by the Google Earth engine which needs vertices to be drawn anticlockwise.
+
+lower_surface, upper_surface = paths.LinePath(*instance_list_lower, height=8000, sort=True), \
                                paths.LinePath(*instance_list_higher, height=15000, sort=True)
 lower_surface.create_sides(upper_surface)
+
+# The ArcPath class allows you to create coordinate instances to draw an 'arc' or 'circle'.  This is based upon a
+# 'central' point referred to as the origin.  The ArcPath will take a start and end bearing and radius from which to
+# plot the arc points.  You can obtain your bearings by using the Coordinate.get_bearing_distance() method.
 
 origin_coordinate = coordinates.Coordinate(55.66, -4.55)
 
@@ -20,10 +38,16 @@ origin_coordinate = coordinates.Coordinate(55.66, -4.55)
 
 arc_path_lower = paths.ArcPath(origin_coordinate, start_bearing=1, end_bearing=359, radius=2, height=200)
 arc_path_higher = paths.ArcPath(origin_coordinate, start_bearing=1, end_bearing=359, radius=2, height=5000)
+
+# The LinePath instance can take all coordinates obtained from the ArcPath class by using the * prefix to iterate
+# through the ArcPath object.
+
 circle_line_path_low = paths.LinePath(*arc_path_lower)
 circle_line_path_high = paths.LinePath(*arc_path_higher)
 circle_line_path_low.create_sides(circle_line_path_high)
 
+
+# You can then use your LinePath instances to render your polygons using SimpleKML.
 
 def create_kml():
     kml = simplekml.Kml()
@@ -58,7 +82,6 @@ def create_kml():
         pol.outerboundaryis = x
         pol.altitudemode = simplekml.AltitudeMode.relativetoground
         i += 1
-
 
     kml.save('..\Floating polygon example.kml')
 
