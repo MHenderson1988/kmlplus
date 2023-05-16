6dec7b72b82f3d26006100eb5901bd1a2f958be7
Mark Henderson <mark.henderson1988@gmail.com>
2023-05-16 16:25:58 +0100

    Aberdeen CTR added

--- kmlplus/kml.py
+++ kmlplus/kml.py
@@ -6,7 +6,9 @@ if __name__ == '__main__':
     curved_layer = ['start=55.2222222 -4.111111 200, end=55.0 -4.1 8000, direction=anticlockwise', '55.2222222 -4.111111']
     straight_layer = ['22.323232 -4.287282 100', '23.323232 -5.328723 150', '22.112333 -6.23789238923 200', '22.323232 -4.287282 100']
 
-    poly = Kml3D(curved_layer_alt_height, curved_layer_alt_height, upper_layer=30000)
+    aberdeen = ['572100N 0023356W', '572100N 0015802W', 'start=571522N 0015428W, end=570850N 0022913W, centre=571207N 0021152W', 'start=571520N 0023326W, end=572100N 0023356W, center=571834N 0021602W']
+
+    poly = Kml3D(aberdeen, aberdeen, upper_layer=11500)
     lower, upper, sides = poly.to_kml()
 
     kml = simplekml.Kml()
